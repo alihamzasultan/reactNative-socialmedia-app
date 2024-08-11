42c4985d98bfb8c939815917a8df12e33534c6f6
alihamzasultan <alihamzasultan6@gmail.com>
2024-08-11 11:53:01 +0500

    Initial commit

---
+++ constants/theme.js
@@ -0,0 +1,33 @@
+export const theme ={
+    colors: {
+        primary: '#00c26f', // Vibrant Green
+        primarydark: '#00ac62', // Dark Green
+        dark: '#3e3e3e', // Dark Gray
+        darkLight: '#555555', // Light Dark Gray
+        gray: '#b0b0b0', // Medium Gray
+    
+        text: '#333333', // Dark Text
+        textLight: '#666666', // Light Gray Text
+        textDark: '#1a1a1a', // Very Dark Text
+    
+        rose: '#ff6f61', // Soft Rose
+        roseLight: '#ff9a8c', // Light Rose
+
+
+    },
+
+    fonts: {
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extraBold: '800',
+    },
+    radius: {
+        xs: 10,
+        sm:12,
+        md:14,
+        lg:16,
+        xl:18,
+        xxl:22,
+    }
+}
